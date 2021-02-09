6950cfd95d219048a7a471c2d85c141f16f4e3de
FunkyXive <andreasnordenhof@gmail.com>
2021-02-09 14:36:39 +0100

    added data models to admin.py

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Car)
+admin.site.register(CarBrand)
+admin.site.register(CarEquipment)
+admin.site.register(Gearbox)
+admin.site.register(Drivetrain)
+admin.site.register(FuelType)
+admin.site.register(CarModel)
+
