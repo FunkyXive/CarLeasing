8ca287fcd5068e452b31eb1d6f1b24d4bf6c7993
FunkyXive <andreasnordenhof@gmail.com>
2021-02-10 15:24:02 +0100

    updated some models added some others

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.db.models import CharField, TextField, ManyToManyField, OneToOneField, ForeignKey, DateField, TimeField, DateTimeField, IntegerField, DecimalField, CASCADE
+import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -25,8 +27,20 @@ class Gearbox(models.Model):
 class Drivetrain(models.Model):
     driveTrain = CharField(max_length=50)
 
+
 class FuelType(models.Model):
     fuelType = CharField(max_length=50, default='Gasoline')
+
+
+class Company(models.Model):
+    contactPerson = ForeignKey(User, on_delete=CASCADE)
+    companyName = CharField(max_length=50)
+    companyStreetName = CharField(max_length=100)
+    companyStreetNumber = IntegerField()
+    companyCity = CharField(max_length=20)
+    companyPostalCode = IntegerField()
+    cvrNumber = IntegerField(max)
+
 
 class CarModel(models.Model):
     modelName = CharField(max_length=50)
@@ -47,3 +61,26 @@ class Car(models.Model):
     model = ForeignKey(CarModel, on_delete=CASCADE)
     milage = IntegerField()
     equipment = ManyToManyField(CarEquipment)
+    carCurrentlyLeased = models.BooleanField(default=False)
+
+
+class PrivateLease(models.Model):
+    leaseStartDate = DateField()
+    leaseEndDate = DateField()
+    leaseDownpayment = DecimalField(max_digits=10, decimal_places=2)
+    leaseMonthlyPrice = DecimalField(max_digits=10, decimal_places=2)
+    leaseCar = OneToOneField(Car, on_delete=CASCADE)
+    leaseCustomer = ForeignKey(User, on_delete=CASCADE)
+
+
+class CompanyLease(models.Model):
+    leaseStartDate = DateField()
+    leaseEndDate = DateField()
+    leaseDownpayment = DecimalField(max_digits=10, decimal_places=2)
+    leaseMonthlyPrice = DecimalField(max_digits=10, decimal_places=2)
+    leaseCar = OneToOneField(Car, on_delete=CASCADE)
+    leaseCustomerCompany = ForeignKey(Company, on_delete=CASCADE)
+
+    
+#class Contract(moodels.Model):
+#    contract = models.FileField() //todo
